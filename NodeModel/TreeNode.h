9a8df690f1e7b91646f13f97f5518769da51fe58
Emily Orton <eort3611@740s-pl-177.local>
2016-04-11 12:10:43 -0600

    Completed TreeNode.h

--- NodeModel/TreeNode.h
+++ NodeModel/TreeNode.h
@@ -10,5 +10,26 @@
 #define TreeNode_hpp
 
 #include "Node.h"
+template <class Type>
+class TreeNode : public Node<Type>
+{
+private:
+    TreeNode<Type> * leftChild;
+    TreeNode<Type> * rightChild;
+    TreeNode<Type> * parent
+    
+public:
+    TreeNode();
+    TreeNode(const Type& value);
+    TreeNode(const Type& value, TreeNode<Type> * parent);
+    
+    void setLeftChild( TreeNode<Type> * leftChild);
+    void setRightChild( TreeNode<Type> * rightChild);
+    void setParent(TreeNode<Type> * parent);
+    TreeNode<Type> * getLeftChild();
+    TreeNode<Type> * getRightChild();
+    TreeNode<Type> * getParent();
+};
+
 
 #endif /* TreeNode_hpp */
