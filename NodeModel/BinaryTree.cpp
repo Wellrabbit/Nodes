127e02162f7bcd92e5d67b99657892ec7254dd32
Emily Orton <eort3611@740s-pl-177.local>
2016-04-13 12:09:22 -0600

    pre post and in order traversal completed in Binary Tree.cpp

--- NodeModel/BinaryTree.cpp
+++ NodeModel/BinaryTree.cpp
@@ -5,5 +5,38 @@
 //  Created by Orton, Emily on 4/11/16.
 //  Copyright © 2016 CTEC. All rights reserved.
 //
-
+#include <iostream>
+using namespace std;
 #include "BinaryTree.h"
+template <class Type>
+void BinaryTree<Type> :: preorderTraversal(TreeNode<Type> * currentNode)
+{
+    if(currentNode != nullptr)
+    {
+        cout << currentNode->getValue() << " " ;
+        preorderTraversal(currentNode->getLetChild());
+        preorderTraversal(currentNode->getRightChild());
+    }
+}
+template <class Type>
+void BinaryTree<Type> :: inorderTraversal(TreeNode<Type> * currentNode)
+{
+    if(currentNode != nullptr)
+    {
+        
+        preorderTraversal(currentNode->getLetChild());
+        cout << currentNode->getValue() << " " ;
+        preorderTraversal(currentNode->getRightChild());
+    }
+}
+template <class Type>
+void BinaryTree<Type> :: postorderTraversal(TreeNode<Type> * currentNode)
+{
+    if(currentNode != nullptr)
+    {
+        
+        preorderTraversal(currentNode->getLetChild());
+        preorderTraversal(currentNode->getRightChild());
+        cout << currentNode->getValue() << " " ;
+    }
+}
